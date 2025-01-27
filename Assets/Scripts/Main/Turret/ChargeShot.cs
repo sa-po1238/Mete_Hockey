0b925df5663e557b7096840edbed2c39f8b5ce47
wasa_byuuu <60276126.taguchi.cgp@gmail.com>
2025-01-28 07:00:26 +0900

    [update]⚡️止まったﾁｬｼｮの消滅
    速度が落ちすぎたチャージショットを破壊するようにしました

--- Assets/Scripts/Main/Turret/ChargeShot.cs
+++ Assets/Scripts/Main/Turret/ChargeShot.cs
@@ -7,6 +7,8 @@ public class ChargeShot : MonoBehaviour
     [SerializeField] private float destroyRightLimit = 12f; // 右側の限界値
     [SerializeField] private float destroyLeftLimit = -12f; // 左側の限界値
     private Rigidbody rb;
+    [SerializeField] private float speedThreshold = 0.01f; // 速度の閾値
+    private float currentTime = 0f;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -18,10 +20,17 @@ public class ChargeShot : MonoBehaviour
 
     void Update()
     {
+        currentTime += Time.deltaTime;
+
         // x方向での範囲チェック
         if ((Mathf.Abs(transform.position.x) > destroyRightLimit) || (Mathf.Abs(transform.position.x) < destroyLeftLimit))
         {
             Destroy(gameObject); // 範囲外に出たら弾丸を破壊
+        }
+        // 速度が落ちすぎたオブジェクトを破壊
+        if ((rb.velocity.magnitude <= speedThreshold) && (currentTime > 1f))
+        {
+            Destroy(gameObject);
         }
     }
 }
