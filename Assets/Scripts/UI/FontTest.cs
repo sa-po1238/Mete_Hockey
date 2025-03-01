9f81e2aac433d06ead8aabe492b801d6bfd99681
sa-po1238 <60276072.kikuchi.cgp@gmail.com>
2025-03-01 22:26:46 +0900

    [add] ✨ 自作フォントと専用コード実装

---
+++ Assets/Scripts/UI/FontTest.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FontTest : MonoBehaviour
+{
+    public int spriteNumber;
+    public GameObject textDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (textDisplay == null)
+        {
+            Debug.LogError("textDisplay がセットされていません");
+            return;
+        }
+
+        TextMeshProUGUI textComponent = textDisplay.GetComponent<TextMeshProUGUI>();
+        if (textComponent == null)
+        {
+            Debug.LogError("textDisplay に TextMeshProUGUI コンポーネントがアタッチされていません");
+            return;
+        }
+
+        spriteNumber = 12345;
+        SetSpriteNumber(spriteNumber, textComponent);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetSpriteNumber(int spriteNumber, TextMeshProUGUI textComponent)
+    {
+        string spriteText = spriteNumber.ToString();
+        textComponent.text = "";
+        for (int i = 0; i < spriteText.Length; i++)
+        {
+            int spriteIndex = int.Parse(spriteText[i].ToString());
+            textComponent.text += "<sprite=" + spriteIndex + ">";
+        }
+    }
+}
