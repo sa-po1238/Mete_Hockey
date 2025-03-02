d352bce2e0b4531b41bea650b34fe4fc8ea4c1d7
sa-po1238 <60276072.kikuchi.cgp@gmail.com>
2025-03-02 14:06:01 +0900

    [add] ✨ 歩行、ダメージアニメーション完成

---
+++ Assets/Scripts/UI/AnimationTest.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTest : MonoBehaviour
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetBool("isWalking", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage()
+    {
+        animator.SetTrigger("isDamaged");
+    }
+
+    
+}
