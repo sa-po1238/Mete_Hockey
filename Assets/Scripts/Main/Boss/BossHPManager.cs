372df1d9fe2a9444447b8f2dbac1ae4d0958800e
wasa_byuuu <60276126.taguchi.cgp@gmail.com>
2025-05-09 04:55:19 +0900

    [add] ✨ボス自体のHPできた
    HPが0になったらデバックログするようにだけしてます

---
+++ Assets/Scripts/Main/Boss/BossHPManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHPManager : MonoBehaviour
+{
+    [SerializeField] CoreActivator coreActivator; // コアのアクティブ化を管理するスクリプト
+    [SerializeField] private float currentBossHP; // 現在のボスHP
+    [SerializeField] private float bossHP = 200f; // ボスのHP
+    private float singleDamage = 5f; // シングルショットのダメージ
+    private float chargeDamage = 30f; // チャージショットのダメージ
+    private float enemyDamage = 50f; // エネミーショットのダメージ
+
+    void Awake()
+    {
+        currentBossHP = bossHP; // ボスのHPを初期化
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (coreActivator.isCoreActive)
+        {
+            if (other.gameObject.CompareTag("SingleShot"))
+            {
+                currentBossHP -= singleDamage;
+            }
+            else if (other.gameObject.CompareTag("ChargeShot"))
+            {
+                currentBossHP -= chargeDamage;
+            }
+            else if (other.gameObject.CompareTag("EnemyShot"))
+            {
+                currentBossHP -= enemyDamage;
+            }
+        }
+        CheckBossHP(); // ボスのHPをチェック
+    }
+    private void CheckBossHP()
+    {
+        if (currentBossHP <= 0)
+        {
+            Debug.Log("ボスが倒されました");
+        }
+    }
+
+}
